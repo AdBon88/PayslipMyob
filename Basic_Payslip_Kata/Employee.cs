dc2eb977a95d8baf7a790ca7e337b2202ffbea48
Adam Bonarrigo <adam.bonarrigo@myob.com>
2020-03-03 18:07:26 +1100

    * .gitignore: First commit
    * Basic_Payslip_Kata.sln:
    * Program.cs:
    * Employee.cs:
    * Basic_Payslip_Kata.csproj:

---
+++ Basic_Payslip_Kata/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Basic_Payslip_Kata
+{
+    public class Employee
+    {
+        private String firstName;
+        private String surname;
+        private int annualSalary;
+        private int superRate;
+
+        public Employee(String firstName, String surname, int annualSalary, int superRate)
+        {
+            this.firstName = firstName;
+            this.surname = surname;
+            this.annualSalary = annualSalary;
+            this.superRate = superRate;
+        }
+    }
+}
---
+++ Basic_Payslip_Kata/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Basic_Payslip_Kata
+{
+    class Program //should this be renamed?
+    {
+ 
+
+        static void Main(string[] args)
+        {
+            //ask about this
+            PayslipGenerator payslipGenerator = new PayslipGenerator();
+            payslipGenerator.generate();
+        }
+    }
+}
