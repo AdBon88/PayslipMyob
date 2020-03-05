7f5446d3906f26f809d3687c523b12aa121bcd48
Adam Bonarrigo <adam.bonarrigo@myob.com>
2020-03-05 14:08:03 +1100

    This file was missing for some reason?

---
+++ Basic_Payslip_Kata/PayslipGenerator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace Basic_Payslip_Kata {
+    public class PayslipGenerator {
+        private Employee employee;
+        private Payslip payslip;
+
+        public PayslipGenerator() {
+        }
+
+        public void start(){
+            employee = getEmployeeDetailsFromUser();
+            payslip = generatePayslip();
+            Console.WriteLine("Your payslip has been generated:\n");
+            Console.WriteLine(payslip.toString());
+            Console.WriteLine("\nThank you for using MYOB!");   
+        }
+
+        private Employee getEmployeeDetailsFromUser() {
+            string firstName;
+            string surname;
+            int annualSalary;
+            int superRate;
+
+            Console.WriteLine("Welcome to the payslip generator!");
+            Console.WriteLine("");
+
+            Console.WriteLine("Please enter your name:");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your surname:");
+            surname = Console.ReadLine();
+
+
+            Console.WriteLine("Please enter your annual salary:");
+            while (!int.TryParse(Console.ReadLine(), out annualSalary)) {
+                Console.WriteLine("Please Enter a valid numerical value!");
+            }
+        
+            Console.WriteLine("Please enter your super rate:");
+            while (!int.TryParse(Console.ReadLine(), out superRate)){
+                Console.WriteLine("Please Enter a valid numerical value!");
+            }
+
+            return new Employee(firstName, surname, annualSalary, superRate);
+        }
+
+        private Payslip generatePayslip() {
+
+            Payslip payslip = getPayslipDatesFromUser(employee);
+
+            return payslip;
+
+        }
+        private Payslip getPayslipDatesFromUser(Employee employee) {
+            DateTime startDate;
+            DateTime endDate;
+
+            Console.WriteLine("Please enter your payment start date:");
+            while (!DateTime.TryParse(Console.ReadLine(), out startDate)) {
+                Console.WriteLine("Please enter a valid date! Eg. 6 March or 03/06/2020");
+            }
+
+            Console.WriteLine("Please enter your payment end date:");
+            while (!DateTime.TryParse(Console.ReadLine(), out endDate)) {
+                Console.WriteLine("Please enter a valid date! Eg. 6 March or 03/06/2020");
+            }
+
+            return new Payslip(employee, startDate, endDate);
+        }
+    }
+}
